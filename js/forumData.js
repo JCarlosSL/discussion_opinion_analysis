5375e57a09f64b97621ebe444e2883e637112e01
Kevin Salazar <kast46.15@gmail.com>
2020-08-05 19:31:07 -0500

    para 5 sentimientos

--- js/forumData.js
+++ js/forumData.js
@@ -2,7 +2,7 @@
 var forumData = function () {
 
 var threadIdList =[];
-var opinionMeta = ['For','Against','Not Sure'];
+var opinionMeta = ['A','B','C','D','E'];//['For','Against','Not Sure'];
 var clusterData = [];
 var clusterMeta = [];
 var maxReply = 0;
@@ -63,7 +63,7 @@ function processCluster(clusterRaw){
 
  
 function processAnnotationData(rawData){
-	var opinionPostCount=[0,0,0];
+	var opinionPostCount=[0,0,0,0,0];//[0,0,0];
 	threadIdList = threadContentData.map(function(d){
 		return d.id;
 	});
@@ -96,7 +96,7 @@ function processAnnotationData(rawData){
 			continue;
 		}
 		pData.unitId = data._unit_id;
-		pData.opinion = data.opinion_toward_the_above_topic;
+		pData.opinion = data.opinion_toward_the_above_topic;//opinionMeta[Math.floor(Math.random()*5)];
 		var confName = 'opinion_toward_the_above_topic:confidence';
 		pData.opinionConf = Number(data[confName]);
 		var oindex = opinionMeta.indexOf(pData.opinion);
