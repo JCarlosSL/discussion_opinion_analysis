eef959a2893604e27ab2de17b20cf6c2fe80cd7c
Jean Carlos soncco lupa <Jean Carlos soncco lupa>
2020-08-13 13:35:22 -0500

    anotaciones

--- ThreadContent.py
+++ ThreadContent.py
@@ -1,32 +1,36 @@
 import csv
 import numpy as np
 from datetime import datetime
-
+import pandas as pd
 def toTimestamp(date):
     datetime_object=datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
     timestamp = datetime.timestamp(datetime_object)
     return str(timestamp)
 
 data = []
+"""
 with open('thread5.tsv',encoding='utf-8') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
     for row in reader:
         data.append(row)
+"""
+df = pd.read_csv('thread5.tsv',sep='\t')
+
 
 data = np.array(data)
 filee = open("threadContent.json", "w")
 filee.write("var threadContentData = [\n")
     
 filee.write("{\"post\":[\n")
-for row in data:
-    filee.write("{ \"topic\":\""+row[0]+"\",\n")
-    filee.write("\t\t\"thread_id\":\""+row[1]+"\",\n")
-    filee.write("\t\t\"post_id\":"+row[2]+",\n")
-    filee.write("\t\t\"time\":"+toTimestamp(row[4])+",\n")
-    filee.write("\t\t\"content\":\""+row[5]+"\",\n")
-    filee.write("\t\t\"user_id\":\""+row[3]+"\",\n")
-    filee.write("\t\t\"replyBy\": ["+row[6]+"],\n")
-    filee.write("\t\t\"replyTo\": ["+row[7]+"]},\n")
+for id,row in df.iterrows():
+    filee.write("{ \"topic\":\""+row['Topic']+"\",\n")
+    filee.write("\t\t\"thread_id\":\""+str(row['Thread ID'])+"\",\n")
+    filee.write("\t\t\"post_id\":"+str(row['post-id'])+",\n")
+    filee.write("\t\t\"time\":"+toTimestamp(row['Time'])+",\n")
+    filee.write("\t\t\"content\":\""+row['Post Content']+"\",\n")
+    filee.write("\t\t\"user_id\":\""+row['User ID']+"\",\n")
+    filee.write("\t\t\"replyBy\": ["+str(row['replyTo'])+"],\n")
+    filee.write("\t\t\"replyTo\": ["+str(row['replyBy'])+"]},\n")
 filee.write("],")
 filee.write("\"id\":\"5\"}]")
 filee.close()
